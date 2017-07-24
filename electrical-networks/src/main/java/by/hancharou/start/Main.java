c08cce27b0fb3397fb41610f0d53c03cd7219e76
User <User@Mres00.mes.minsk.energo.net>
2017-07-24 15:24:32 +0300

    I have deleted all, what I didn't need.

--- electrical-networks/src/main/java/by/hancharou/start/Main.java
+++ electrical-networks/src/main/java/by/hancharou/start/Main.java
@@ -5,47 +5,27 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.apache.commons.dbcp2.BasicDataSource;
-
 import by.hancharou.dbcp.ConnectionManager;
 
 
 public class Main {
-	private BasicDataSource dataSource;
-
-	private BasicDataSource getDataSource() {
-		if (dataSource == null) {
-			BasicDataSource ds = new BasicDataSource();
-			ds.setUrl("jdbc:mysql://localhost/electrical_networks");
-			ds.setUsername("root");
-			ds.setPassword("pass");
-			ds.setMinIdle(5);
-			ds.setMaxIdle(10);
-			ds.setMaxOpenPreparedStatements(100);
-			dataSource = ds;
-		}
-		return dataSource;
-	}
-
+	private Connection connection;
+	private PreparedStatement pStatement;
+	private ResultSet resultSet;
+	
 	public void sqlTest() throws SQLException, ClassNotFoundException {
-		Class.forName("com.mysql.jdbc.Driver");
-		try (BasicDataSource dataSource = getDataSource();
-				/*Connection connection = dataSource.getConnection();
-				PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM tbl_ps;");*/
-				Connection connection = ConnectionManager.getManager().getConnection();
-				//Connection connection = dataSource.getConnection();
-				PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM tbl_ps;");
-				) {
-			System.out.println("The Connection Object is of Class: " + connection.getClass());
-			try (ResultSet resultSet = pstmt.executeQuery();) {
-				while (resultSet.next()) {
-					System.out.println(resultSet.getInt(1) + resultSet.getString(2));
-				}
-			} catch (Exception e) {
-				connection.rollback();
-				e.printStackTrace();
-			} 
-		} 
+		try{
+			connection = ConnectionManager.getManager().getConnection();
+			pStatement = connection.prepareStatement("SELECT * FROM tbl_ps;");
+			resultSet = pStatement.executeQuery();
+			
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt(1) + resultSet.getString(2));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionManager.getManager().closeDbResources(connection, pStatement, resultSet);
+		}
 	}
-
 }
--- electrical-networks/src/main/java/by/hancharou/start/MyServlet.java
+++ electrical-networks/src/main/java/by/hancharou/start/MyServlet.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-//@WebServlet("/my-servlet")
 public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
